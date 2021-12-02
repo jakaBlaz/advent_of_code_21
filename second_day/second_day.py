1215b898753b4148e4e75e9a2808311b4a4c84d4
jakaBlaz <blaz.molicnik@gmail.com>
2021-12-02 08:51:19 +0100

    second day organised

--- second_day/second_day.py
+++ second_day/second_day.py
@@ -1,21 +1,38 @@
-direction = ""
-value = 0
-depth = 0
-horizontal = 0
-aim = 0
+def first_part():
+    direction = ""
+    value = 0
+    depth = 0
+    horizontal = 0
+    with open("input.txt",'r') as f:
+        for i in f:
+            direction, value =  i.split(" ")
+            value = int(value)
+            if direction == "forward":
+                horizontal += value
+            elif direction == "down":
+                depth += value
+            elif direction == "up":
+                depth -= value
+    return depth*horizontal
 
-with open("input.txt",'r') as f:
-    for i in f:
-        direction, value =  i.split(" ")
-        value = int(value)
-        if direction == "forward":
-            horizontal += value
-            depth += aim * value
-        elif direction == "down":
-            aim += value
-        elif direction == "up":
-            aim -= value
-            
-print(f'depth is {depth}')
-print(f'with a horizontal distance of {horizontal}')
-print(f' Multiplied together they are {depth * horizontal}')
+def second_part():
+    direction = ""
+    value = 0
+    depth = 0
+    horizontal = 0
+    aim = 0
+    with open("input.txt",'r') as f:
+        for i in f:
+            direction, value =  i.split(" ")
+            value = int(value)
+            if direction == "forward":
+                horizontal += value
+                depth += aim * value
+            elif direction == "down":
+                aim += value
+            elif direction == "up":
+                aim -= value
+    return depth*horizontal
+
+print(f"The answer to the first part is: {first_part()}")
+print(f"The answer to the first part is: {second_part()}")
